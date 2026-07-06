6979e7324c3fe98e1c535675e46fc97724aad6f8
gerardreyandrewvalenzuela-sketch <gerardreyandrewvalenzuela@gmail.com>
2026-07-06 22:52:24 +0800

    Add Apps Script deployment URL

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,7 +4,7 @@
 // ============================================================
 
 // TODO: Replace this URL after you deploy your Apps Script as a Web App
-var API_URL = 'https://script.google.com/macros/s/AKfycby_HM635vwlHG_WKbh8d-KShFzgNP3PhfmwshgoiR9v/dev';
+var API_URL = 'https://script.google.com/macros/s/AKfycbwk8D_oQ5g6ykbsRi0qggS_v7b8V7Z7FLrzd-NqyG_mBlwXrxSBs7_oukQ1PoYOo6uGjA/exec';
 
 // ── Stored PIN (session-level) ───────────────────────────────
 var _pin = sessionStorage.getItem('hmsams_pin') || '';
